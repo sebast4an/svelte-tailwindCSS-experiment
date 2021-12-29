22ac8bd1ccc26545f885fd4e7dcff8d999f0b482
sebast4an <szczypinskisebastian@gmail.com>
2021-12-29 13:19:00 +0100

    first components

---
+++ src/components/atoms/Button/Button.svelte
@@ -0,0 +1,9 @@
+<script>
+  const handleClick = () => {
+    alert('Work');
+  };
+</script>
+
+<button class="rounded-xl border-0 bg-zinc-700 py-3 px-8" on:click={handleClick}>
+Handle Click
+</button>
---
+++ src/components/pages/Main.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Button from '../atoms/Button/Button.svelte';
+</script>
+
+<header class="w-100 h-auto flex justify-center mt-10">
+  <h1 class="font-bold text-2xl">Svelte calculator</h1>
+</header>
+
+<main>
+    <section>
+        308 x 42
+    </section>
+
+    <section>
+        12,936
+    </section>
+</main>
+
+<div>
+    <Button />
+</div>
