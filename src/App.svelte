5708b9e3e3a8160e58b41f912dbd769991249ad7
sebast4an <szczypinskisebastian@gmail.com>
2021-12-29 10:33:36 +0100

    setup

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let name;
+</script>
+
+<main class="lg:text-lg">
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.
+  </p>
+</main>
+
+<style type="text/postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
